bbbdb93c8ab68d9276406ef1625e5f6589401be0
Jule <julischka@onlinehome.de>
2020-01-19 22:00:48 +0100

    draw divs in correct size

--- js/base-demo.js
+++ js/base-demo.js
@@ -10,7 +10,7 @@ export default {
   init: function () {
     let sortSizeInput = document.querySelector('#size-input');
     sortSizeInput.addEventListener('change', () => {
-      this.generateData(sortSizeInput.value);
+      this.generateData();
       this.canvasDrawer.draw(this.values);
     });
 
@@ -36,14 +36,43 @@ export default {
       }
     });
 
-    this.generateData(this.initData);
+    this.generateData();
     this.canvasDrawer.draw(this.values);
   },
 
-  generateData: function (size) {
+  generateData: function () {
+    let cols = 60;
+    let rows = 40;
     this.values = [];
-    for (let i = 0; i < size; i++) {
-      this.values.push(Math.random() * 400);
+    let id = 0;
+    for (let row = 0; row < rows; row++) {
+      let rowArr = [];
+      for (let col = 0; col < cols; col++) {
+        let val = {
+          id: id++
+        };
+        let children = [];
+        // right
+        if (col < cols - 1) {
+          children.push({ weight: 1, childId: val.id + 1 });
+        }
+        // left
+        if (col > 0) {
+          children.push({ weight: 1, childId: val.id - 1 });
+        }
+        // bottom
+        if (row < rows - 1) {
+          children.push({ weight: 1, childId: val.id + cols });
+        }
+        // top
+        if (row > 0) {
+          children.push({ weight: 1, childId: val.id - cols });
+        }
+        val.children = children;
+        rowArr.push(val);
+      }
+      this.values.push(rowArr);
     }
+    console.table(this.values);
   }
 };
--- js/canvas-drawer.js
+++ js/canvas-drawer.js
@@ -1,19 +1,29 @@
 class CanvasDrawer {
   constructor(canvasParentID) {
     this.parent = document.getElementById(canvasParentID);
-    let width = this.parent.clientWidth;
-    let height = this.parent.clientHeight;
-    const divSize = 20;
-    for (let i = 0; i < Math.floor((width * height) / (divSize * divSize)); i++) {
-      var newDiv = document.createElement("div");
-      newDiv.setAttribute("id", i);
-      newDiv.setAttribute("class", "newDiv");
-      this.parent.appendChild(newDiv);
-    }
+    this.parentWidth = this.parent.clientWidth;
+    this.parentHeight = this.parent.clientHeight;
   }
 
   draw (values) {
     console.log('draw');
+    const divSize = this.parentWidth / values[0].length;
+    let id = 0;
+    for (let row = 0; row < values.length; row++) {
+      let rowDiv = document.createElement('div');
+      for (let col = 0; col < values[0].length; col++) {
+        let newDiv = document.createElement('div');
+        newDiv.id = id++;
+        newDiv.setAttribute('class', 'square');
+        newDiv.style.width = divSize;
+        newDiv.style.height = divSize;
+        newDiv.addEventListener('click', function () {
+          newDiv.classList.toggle('clicked');
+        });
+        rowDiv.appendChild(newDiv);
+      }
+      this.parent.appendChild(rowDiv);
+    }
   }
 }
 module.exports = CanvasDrawer;
